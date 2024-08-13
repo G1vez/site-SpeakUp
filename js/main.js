7b62b9cae773e20a28d60faab343a490cc6fc430
G1vez <gjhrdxvju@gmail.com>
2024-08-13 22:03:40 +0300

    новий дизайн, почищений код

--- js/main.js
+++ js/main.js
@@ -56,20 +56,22 @@ document.addEventListener("touchend", function(event) {
 });
 
 // автопрокручування слайд-шоу
-slideIndex = 0;
-carousel();
+// slideIndex = 0;
+// carousel();
 
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > x.length) {slideIndex = 1}
-  x[slideIndex-1].style.display = "block";
-  setTimeout(carousel, 15000); // Change image every 2 seconds
-}
+// function carousel() {
+//   var i;
+//   var x = document.getElementsByClassName("mySlides");
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none";
+//   }
+//   slideIndex++;
+//   if (slideIndex > x.length) {slideIndex = 1}
+//   x[slideIndex-1].style.display = "block";
+//   setTimeout(carousel, 15000); // Change image every 2 seconds
+// }
+
+
 
 // ----------------------------------------------------------------------
 
