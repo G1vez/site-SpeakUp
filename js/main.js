605f1b803914f1d1299c10b74becbdcf0ffa3cf5
G1vez <gjhrdxvju@gmail.com>
2024-08-18 17:20:09 +0300

    нове меню, слайди, шаблони, перемикач мови, повернення кнопки меню,

--- js/main.js
+++ js/main.js
@@ -14,17 +14,12 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
 } 
 
 let touchStartX = 0;
@@ -74,13 +69,3 @@ document.addEventListener("touchend", function(event) {
 
 
 // ----------------------------------------------------------------------
-
-// Отримати кнопку та контейнер меню
-const navToggle = document.querySelector('.nav-toggle');
-const navContent = document.querySelector('.nav-content');
-
-// Додати обробник події натиску кнопки
-navToggle.addEventListener('click', () => {
-  // Переключити клас 'open' на контейнері меню
-  navContent.classList.toggle('open');
-});
