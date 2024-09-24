fde33ba6ebff232b7124d905c9f763a121bab811
G1vez <gjhrdxvju@gmail.com>
2024-09-25 00:07:30 +0300

    import json to js file

--- js/card-loader.js
+++ js/card-loader.js
@@ -1,33 +1,34 @@
-$.ajax({
+function createCardHTML(item) {
+  return `
+    <figure>
+      <a href=" ">
+        <img src="${item.image_url}" alt="${item.title}">
+      </a>
+    </figure>
+    <div>
+      <a href=" " class="article-text black">${item.title}</a>
+      <div class="gray text-card">${item.intro}</div>
+    </div>
+  `;
+}
+
+fetch({
   type: 'GET',
   url: 'http://127.0.0.1:8000/articles/',
-  dataType: 'json',
-  success: function(data) {
-    console.log(data);
-    const numDataItems = data.length;
-    const numItemsToProcess = Math.min(numLoadingPlaceholders, numDataItems);
-
-    for (let i = 0; i < numItemsToProcess; i++) {
-        const item = data[i];
-        const cardHTML = `
-          <figure>
-            <a href=" ">
-              <img src="${item.image_url}" alt="${item.title}">
-            </a>
-          </figure>
-          <div>
-            <a href=" " class="article-text black">${item.title}</a>
-            <div class="gray text-card">${item.intro}</div>
-          </div>
-        `;
-      
-        $('#cards-container').append(cardHTML);
-        if (i === 0) {
-          $('#cards-container > :eq(0)').attr('id', 'square');
-        }
-      }
-    },
-  error: function(xhr, status, error) {
-    console.error('Error:', error);
-  }
+  dataType: 'json'
+})
+.then(response => response.json())
+.then(data => {
+  data.forEach(item => {
+    cardsContainer.innerHTML += createCardHTML(item);
+  });
+})
+.catch(error => {
+  fetch('http://127.0.0.1:8000/podojg.json')
+    .then(response => response.json())
+    .then(json => {
+      json.forEach(item => {
+        cardsContainer.innerHTML += createCardHTML(item);
+      });
+    });
 });
