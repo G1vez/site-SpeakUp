730a621f102365f16ee1fef020f0bdd602b52d64
G1vez <gjhrdxvju@gmail.com>
2024-10-07 10:19:15 +0300

    allCategory in footer

--- js/allCategory.js
+++ js/allCategory.js
@@ -1,38 +1,66 @@
-function createCardHTML(item, index) {
+function createSubmenuHTML(item) {
   return `
-    <div class="card">
-      <figure>
-        <a href="">
-          <img ${index === 0 ? 'id="square"' : ''} src="${item.image_url}" alt="${item.title}">
-        </a>
-      </figure>
-      <div>
-        <a href="" class="article-text black">${item.title}</a>
-        <div class="gray text-card">${item.intro}</div>
-      </div>
-    </div>
+    <li><a href="./specific_category.html" data-category="${item.slug}">${item.name}</a></li>
   `;
 }
-const cardsContainer = document.getElementById('cards-container');
+const specialCategories = document.querySelector('.submenu #special-categories');
 fetch({
   type: 'GET',
-  url: 'http://127.0.0.1:8000/articles/',
+  url: 'http://127.0.0.1:8000/categories/',
   dataType: 'json'
 })
 .then(response => response.json())
 .then(data => {
-  cardsContainer.innerHTML = ''; // clear the container
-  data.results.forEach((item, index) => {
-    cardsContainer.innerHTML += createCardHTML(item, index);
+  specialCategories.innerHTML = ''; // clear the container
+  data.results.forEach((item) => {
+    specialCategories.innerHTML += createSubmenuHTML(item);
   });
 })
 .catch(error => {
-  fetch('http://127.0.0.1:5500/locally/podojg.json')
+  fetch('http://127.0.0.1:5500/locally/allCategory.json')
     .then(response => response.json())
     .then(json => {
-      cardsContainer.innerHTML = ''; // clear the container
-      json.results.forEach((item, index) => {
-        cardsContainer.innerHTML += createCardHTML(item, index);
+      specialCategories.innerHTML = ''; // clear the container
+      json.results.forEach((item) => {
+        specialCategories.innerHTML += createSubmenuHTML(item);
       });
     })
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const footerList = document.querySelector('.footer-list');
+  fetch('http://127.0.0.1:8000/categories/')
+    .then(response => response.json())
+    .then(data => {
+      const newItems = data.results.map(item => {
+        return `
+          <li><a href="./specific_category.html" data-category="${item.slug}">${item.name}</a></li>
+        `;
+      });
+      footerList.innerHTML = '';
+      footerList.innerHTML += `
+        <li class=""><a href="">Про нас</a></li>
+        <li class=""><a href="">Історії</a></li>
+        ${newItems.join('')}
+        <li class=""><a href="">Корисні посилання</a></li>
+      `;
+    })
+    .catch(error => {
+      fetch('http://127.0.0.1:5500/locally/allCategory.json')
+        .then(response => response.json())
+        .then(json => {
+          const newItems = json.results.map(item => {
+            return `
+              <li><a href="./specific_category.html" data-category="${item.slug}">${item.name}</a></li>
+            `;
+          });
+          footerList.innerHTML = '';
+          footerList.innerHTML += `
+            <li class=""><a href="">Про нас</a></li>
+            <li class=""><a href="">Історії</a></li>
+            ${newItems.join('')}
+            <li class=""><a href="">Корисні посилання</a></li>
+          `;
+        })
+    });
 });
